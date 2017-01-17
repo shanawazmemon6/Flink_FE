5a8a3c5ffaec65a68efd093eebb50d0661998a31
Shanawaz Memon <shanawazmemon6@gmail.com>
2017-01-18 00:07:17 +0530

    Completed Blog

--- WebContent/Blog/BlogService.js
+++ WebContent/Blog/BlogService.js
@@ -8,9 +8,43 @@ app.service('BlogService',['$http',function($http){
 			return $http.post(base_url+"createblog",blog).then
 			(function(response) {
 				return response.data;
-				console(response.data)
+				console.log(response.data)
 			},null)
-		}
+		},
+	    
+	        UsersBlog:function(){
+	    	 return $http.get(base_url+"userBlog").then
+	    	 (function(response) {
+	    		 
+
+	    		 return response.data;
+	    		 
+					},null)
+					},
+		
+		deleteBlog:function(blog_id){
+	    	 return $http.get(base_url+"deleteBlog"+"/"+blog_id).then
+	    	 (function(response) {
+	    		 
+
+	    		 return response.data;
+	    		 
+					},null)
+					},
+					editBlog:function(blog_id){
+						return $http.get(base_url+"editBlog"+"/"+blog_id).then
+						(function(response) {
+							return response.data;
+							console.log(response.data)
+						},null)
+					},
+					updateBlog:function(blog){
+						return $http.put(base_url+"createblog",blog).then
+						(function(response) {
+							return response.data;
+							console.log(response.data)
+						},null)
+					},
 		
 	}
 	
--- WebContent/Blog/Blogctrl.js
+++ WebContent/Blog/Blogctrl.js
@@ -1,21 +1,74 @@
 app.controller('BlogController',['BlogService','$rootScope',function(BlogService,$location,$rootScope){
 	
-	var blogs=this;
+	var ctrl=this;
 	
-blogs.blog={blog_id:'',title:'',description:'',writtenby:'',date_blog:'',like_log:'',dislike_blog:'',brief:''}
-
-blogs.saveBlog=function(blog){
+ctrl.blog={blog_id:'',title:'',description:'',writtenby:'',date_blog:'',like_log:'',dislike_blog:'',brief:'',code:'',error:''}
+      ctrl.blogs=[];
+ctrl.upd='';
+ctrl.saveBlog=function(blog){
 BlogService.saveBlog(blog).then(
 	function(d) {
-		blogs.blog=d;
-		console.log(blogs.blog)
+	   console.log(ctrl.blog)
+	   ctrl.fetchUserBlog();
+
 	})}
 
-blogs.submit=function(){
-	blogs.blog.blog_id='102'
-    blogs.blog.date_blog=new Date();
+ctrl.fetchUserBlog=function(){
+	BlogService.UsersBlog().then(
+			function(d) {
+				ctrl.blogs=d;	
+			})}
+
+ctrl.deleteBlog=function(Blog_id){
+	BlogService.deleteBlog(Blog_id).then
+	(function(d){
+		   ctrl.fetchUserBlog();
+
+		console.log(d)
+		
+	})
 	
-	blogs.saveBlog(blogs.blog)
+}
+ctrl.editBlog=function(Blog_id){
+	BlogService.editBlog(Blog_id).then
+	(function(d){
+       ctrl.blog=d;
+       ctrl.modblog=d;
+       ctrl.upd='show'
+		console.log(d)
+		
+	})}
+ctrl.modBlog=function(Blog_id){
+	BlogService.editBlog(Blog_id).then
+	(function(d){
+       ctrl.modblog=d;
+       
+		
+	})}
+ctrl.fetchUserBlog();
+
+ctrl.updateBlog=function(blog){
+	BlogService.updateBlog(blog).then(
+		function(d) {
+			
+		   ctrl.fetchUserBlog();
+
+		})}
+ctrl.submit=function(){
+	if(ctrl.upd=='show'){
+		ctrl.updateBlog(ctrl.blog)
+		ctrl.upd='';
+       ctrl.reset();
+		}else{
+		ctrl.saveBlog(ctrl.blog)
+		       ctrl.reset();
+
+	    ctrl.blog.date_blog=new Date();
+}
+}
+ctrl.reset=function(){
+	ctrl.blog={blog_id:'',title:'',description:'',writtenby:'',date_blog:'',like_log:'',dislike_blog:'',brief:'',code:'',error:''}
+
 	
 }
 
